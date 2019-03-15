81c4439ee19fdadd4863ec4020bca84115dc5a81
张晨辉 <cheney_zhang@foxmail.com>
2019-03-15 20:59:27 +0800

    添加css

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -11,20 +11,19 @@ import React from 'react';
 
 
 import './importAllIcons';
+import './icon.scss';
 
 interface IconProps {
   name: string;
 }
 
-// 如何生命一个react函数组件接受一个类型
+// 如何声明一个react函数组件接受一个类型
 // 生命Icon为react函数组件，接受参数 类型IconProps
 const Icon: React.FunctionComponent<IconProps> = (props) => {
   return (
-    <span>
-      <svg>
-        <use xlinkHref={`#${props.name}`}/>
-      </svg>
-    </span>
+    <svg className="hui-icon">
+      <use xlinkHref={`#${props.name}`}/>
+    </svg>
   );
 };
 
