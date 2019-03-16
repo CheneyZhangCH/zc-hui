b166b414f387dd6b9e7a3d20a2a4a2802582a2c5
张晨辉 <cheney_zhang@foxmail.com>
2019-03-16 16:36:21 +0800

    增加注释

--- lib/helper/classnames.tsx
+++ lib/helper/classnames.tsx
@@ -1,5 +1,6 @@
 // 处理组件自身的className 和 用户传来的className 合并的问题
 function classnames (...names: (string | undefined)[]) {
+  // return names.filter(v => v).join(' ') 和下面的方法等价
   return names.filter(Boolean).join(' ')
 }
 export default classnames
