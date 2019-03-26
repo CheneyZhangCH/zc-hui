e739e78ca773b009cf5ef1d6ad233f70dee22bac
张晨辉 <cheney_zhang@foxmail.com>
2019-03-26 20:56:57 +0800

    circleci 集成自动发布

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,23 +1,5 @@
-import React from 'react';
-import ReactDom from 'react-dom';
 import Icon from './icon/icon';
 
 // import Button from './button';
 
-
-const fn: React.MouseEventHandler = (e) => {
-  console.log(e.target);
-  // console.log(e.target as HTMLDivElement); // 断言
-};
-
-ReactDom.render(
-  <div>
-    <Icon
-      name='wechat'
-      onClick={fn}
-      className={'hahaha'}
-      onMouseEnter={() => {console.log('enter')}}
-      onMouseLeave={() => {console.log('leave')}}/>
-  </div>,
-  document.querySelector('#root')
-);
+export {Icon};
