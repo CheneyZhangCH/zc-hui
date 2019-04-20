4f1a7175b047dbb43e96dcf9737a95c240422115
张晨辉 <cheney_zhang@foxmail.com>
2019-04-20 11:08:39 +0800

    按照antd更新icon

--- lib/button/button.example.tsx
+++ lib/button/button.example.tsx
@@ -4,7 +4,28 @@ import Button from "./button";
 const IconExample: React.FunctionComponent = () => {
   return (
     <div>
-      <Button/>
+      <Button style={{ marginRight: 10 }} type="primary">
+        Primary
+      </Button>
+      <Button style={{ marginRight: 10 }} type="text">
+        Text
+      </Button>
+      <Button style={{ marginRight: 10 }} type="default">
+        default
+      </Button>
+      <Button style={{ marginRight: 10 }} type="primary" size="large">
+        large
+      </Button>
+      <Button style={{ marginRight: 10 }} type="text" size="normal">
+        Text
+      </Button>
+      <Button style={{ marginRight: 10 }} type="default" size="small">
+        small
+      </Button>
+      <Button style={{ marginRight: 10 }} shape="circle">
+        circle
+      </Button>
+
     </div>
   );
 };
