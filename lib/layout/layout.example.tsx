490a20f314ac19f99d0dfd6520d7e5680bde68b3
张晨辉 <cheney_zhang@foxmail.com>
2019-04-28 18:35:59 +0800

    修复layout样式错误

--- lib/layout/layout.example.tsx
+++ lib/layout/layout.example.tsx
@@ -1,49 +1,48 @@
 import React from 'react';
 
 import Layout from './layout';
-import Header from "./header";
-import Content from "./content";
-import Footer from "./footer";
-import Aside from "./aside";
+import Header from './header';
+import Content from './content';
+import Footer from './footer';
+import Aside from './aside';
 
 export default function layout() {
   return (
     <div>
       <div>第1个例子</div>
-      <Layout style={{ height: 300 }} >
-        <Header className='test-class test  test1 test2'>Header</Header>
-        <Content className='test-class test  test1 test2'>Content</Content>
-        <Footer className='test-class test  test1 test2'>Content</Footer>
+      <Layout style={{ height: 300 }}>
+        <Header>Header</Header>
+        <Content>Content</Content>
+        <Footer>Footer</Footer>
       </Layout>
 
       <div>第2个例子</div>
-      <Layout style={{ height: 300 }} className='test-class test  test1 test2'>
-        <Header className='test-class test  test1 test2'>Header</Header>
+      <Layout style={{ height: 300 }}>
+        <Header>Header</Header>
         <Layout>
-          <Aside className='test-class test  test1 test2'>Aside</Aside>
-          <Content className='test-class test  test1 test2'>Content</Content>
+          <Aside>Aside</Aside>
+          <Content>Content</Content>
         </Layout>
-        <Footer className='test-class test  test1 test2'>Footer</Footer>
+        <Footer>Footer</Footer>
       </Layout>
 
-
       <div>第3个例子</div>
-      <Layout style={{ height: 300 }} className='test-class test  test1 test2'>
-        <Header className='test-class test  test1 test2'>Header</Header>
+      <Layout style={{ height: 300 }}>
+        <Header>Header</Header>
         <Layout>
-          <Content className='test-class test  test1 test2'>Content</Content>
-          <Aside className='test-class test  test1 test2'>Aside</Aside>
+          <Content>Content</Content>
+          <Aside>Aside</Aside>
         </Layout>
-        <Footer className='test-class test  test1 test2'>Footer</Footer>
+        <Footer>Footer</Footer>
       </Layout>
 
       <div>第4个例子</div>
-      <Layout style={{ height: 300 }} className='test-class test  test1 test2'>
-        <Aside className='test-class test  test1 test2'>Aside</Aside>
+      <Layout style={{ height: 300 }}>
+        <Aside>Aside</Aside>
         <Layout>
-          <Header className='test-class test  test1 test2'>Header</Header>
-          <Content className='test-class test  test1 test2'>Content</Content>
-          <Footer className='test-class test  test1 test2'>Footer</Footer>
+          <Header>Header</Header>
+          <Content>Content</Content>
+          <Footer>Footer</Footer>
         </Layout>
       </Layout>
     </div>
