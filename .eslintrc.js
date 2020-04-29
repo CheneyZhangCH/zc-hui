d1af30b6269b14109c87e0e3e50b48a4740757a3
张晨辉 <cheney_zhang@foxmail.com>
2020-04-29 15:51:51 +0800

    添加eslint prettier husky 及 lint-staged

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+	parser: "@typescript-eslint/parser",
+	extends: [
+		"plugin:@typescript-eslint/recommended",
+		"plugin:prettier/recommended",
+		"plugin:react/recommended",
+		"eslint-config-prettier",
+		"eslint-config-prettier/react",
+		"prettier/@typescript-eslint"
+	],
+	plugins: ["@typescript-eslint", "prettier"],
+	rules: {
+		"prettier/prettier": "error",
+		"@typescript-eslint/explicit-function-return-type": "off",
+		"@typescript-eslint/no-explicit-any": "off",
+		"react/display-name": "off",
+		"@typescript-eslint/explicit-member-accessibility": "off",
+		"@typescript-eslint/interface-name-prefix": "off",
+		"@typescript-eslint/member-delimiter-style": "off",
+		"@typescript-eslint/no-var-requires": "off",
+		"@typescript-eslint/ban-ts-ignore": "off",
+		"react/prop-types": 0,
+		'@typescript-eslint/no-use-before-define': [
+			'error',
+			{ functions: true, classes: true, variables: false },
+		],
+	},
+	settings: {
+		react: {
+			version: "detect"
+		}
+	}
+};
+
